825e7243e2168a8b9190eddf4cfbd209d21c6902
Stephen <stephenvdw7777@gmail.com>
2025-09-02 17:02:36 +0200

    w/Bak - Add prettier formatting - update site icon to use MeetingPal Icon - update site icon to be MeetingPal API - clean up readme - fix wrong method request types - fix endpoint formats - removed unused code - clean up descriptions - add missing endpoints - fix incorrect endpoint properties -
    Co-authored-by: BakBrood <BakBrood@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import { render } from "preact";
-import React from "preact/compat";
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
-import "./style.css";
+import { render } from 'preact';
+import React from 'preact/compat';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+import './style.css';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <SwaggerUI url="swagger.json" />
-    </div>
-  );
+    return (
+        <div>
+            <SwaggerUI url="swagger.json" />
+        </div>
+    );
 };
 
 export default App;
 
-if (typeof window !== "undefined") {
-  render(<App />, document.getElementById("app"));
+if (typeof window !== 'undefined') {
+    render(<App />, document.getElementById('app'));
 }
